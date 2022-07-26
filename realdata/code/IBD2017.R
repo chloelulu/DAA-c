73b2c0c5f3f4f9f380555d268f65a2bf4a3a6066
chloelulu <luyang1005@gmail.com>
2022-07-26 12:25:14 -0500

    update

--- realdata/code/IBD2017.R
+++ realdata/code/IBD2017.R
@@ -11,19 +11,6 @@ source('code/CompetingMethods1.R') ## can adjust Z
 load('Data/IBD_2017_dat.Rdata')
 
 
-# Shuffle the meta data
-# meta <- dat$meta.dat
-# meta.list <- list()
-# for(i in 1:1000){
-#   cat(i,'\n')
-#   meta1 <- as.data.frame(meta %>% rownames_to_column('sam.ids') %>% group_by(X,SubjectID) %>% dplyr::mutate(time=sample(time, replace = F)))
-#   meta1 <- meta1 %>% base::split(meta1$SubjectID) %>% sample(replace = F) %>% bind_rows %>% column_to_rownames('sam.ids')
-#   # # meta1 <- meta1 %>% base::split(meta1$SubjectID) %>% sample %>% base::unsplit(meta1$SubjectID)
-#   # meta1$X.old <- meta$X
-#   # meta1$T.old <- meta$time
-#   meta.list[[i]] <- meta1
-# }
-# save(meta.list, file = 'Data/dat.IBD2017.shuffle1000.meta.Rdata')
 
 ## Shuffle disease status
 # meta <- dat$meta.dat
@@ -41,41 +28,6 @@ load('Data/IBD_2017_dat.Rdata')
 # }
 # save(meta.list, file = 'Data/dat.IBD2017.shuffle1000.meta3.Rdata')
 
-
-# shuffle by sample by ratio
-# meta <- dat$meta.dat
-# meta.list <- list()
-# for(i in 1:1000){
-#   cat(i,'\n')
-#   meta1 <- meta[meta$X==1,] %>% rownames_to_column('sam.ids') %>% group_by(SubjectID)
-#   meta0 <- meta[meta$X==0,] %>% rownames_to_column('sam.ids') %>% group_by(SubjectID)
-#   table(meta$X)[1]/nrow(meta)
-#   ## sampling by ratio
-#   idx.C <- sample(unique(meta1$SubjectID), (table(meta$X)[1]/nrow(meta)) * length(unique(meta$SubjectID)), replace = F)
-#   idx.T <- setdiff(unique(meta$SubjectID), idx.C)
-#   meta.C <- meta[meta$SubjectID %in% idx.C,] %>% rownames_to_column('sam.ids') %>% group_by(SubjectID) %>% mutate(X =0)
-#   meta.T <- meta[meta$SubjectID %in% idx.T,] %>% rownames_to_column('sam.ids') %>% group_by(SubjectID) %>% mutate(X =1)
-# 
-#   ## shuffle time in each subject
-#   meta.CT <- meta.C %>% group_by(X,SubjectID) %>% dplyr::mutate(time=sample(time, replace = F)) %>% as.data.frame
-#   meta.TT <- meta.T %>% group_by(X,SubjectID) %>% dplyr::mutate(time=sample(time, replace = F)) %>% as.data.frame
-# 
-#   meta.new <- rbind(meta.CT , meta.TT) %>% group_by(X,SubjectID) %>% as.data.frame %>% column_to_rownames('sam.ids')
-#   meta.list[[i]] <- meta.new
-# }
-# save(meta.list, file = 'Data/dat.IBD2017.shuffle1000.meta2.Rdata')
-
-
-## try another type of shuffle
-# meta <- dat$meta.dat
-# meta.list <- list()
-# for(i in 1:1000){
-#   nm <- sample(rownames(meta), replace = F)
-#   meta1 <- meta
-#   rownames(meta1) <- nm
-#   meta.list[[i]] <- meta1
-# }
-# save(meta.list, file = 'Data/dat.IBD2017.shuffle1000.meta1.Rdata')
 
 
 methods_funs <- list('glmernb'='glmernb.func','lme4'='lme4.func','GLMMPQL'='GLMMPQL.func','ZIBR'='ZIBR.func','LDM'='LDM.func',
---
+++ realdata/code/README.txt
@@ -0,0 +1,4 @@
+
+IBD2017.R, Nicholas_2013.R and smoker.R are the code for generate shuffled metadata + real data analysis
+shuffle_smoker.R, shuffle_Nicholas2013.R, shuffle_IBD2017.R are scripts for analyzing shuffled data 1000 times
+
